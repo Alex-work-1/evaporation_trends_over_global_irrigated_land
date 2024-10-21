0ef058ede86bbf5d67bc50d4b4a919510016113d
Alex-work-1 <xhola030@studenti.czu.cz>
2024-10-21 02:21:20 +0200

    Added LOESS smoothing

--- R/03_trend_analysis.R
+++ R/03_trend_analysis.R
@@ -1,5 +1,5 @@
 ##
-#Step 3: Slope calculations [global cropland trend]
+#Step 3: Slope calculations [global annual cropland trend] (linear regression and loess)
 ##
 source("./source/main.R")
 
@@ -22,16 +22,16 @@ for(evap_data_path in evap_data_paths){
   # convert to data table format
   gleam_datatable <- as.data.table(gleam_dataframe)
   
-  # convert text date into date object 
-  gleam_datatable[, Date := as.Date(Date, format = "%Y-%m-%d")]
-  
+  # convert text date into date object
+  gleam_datatable[, Date := as.Date(Date, format = "%Y-%m-%d")] 
   
   # --- Step 4: Linear trend analysis  ---
   # get the world evapotranspiration mean by date
   gleam_datatable_mean <- gleam_datatable %>%
     na.omit() %>%
     group_by(Date) %>%
-    summarise(`Mean evapotranspiration [mm]` = mean(`evap`))
+    summarise(`Mean evapotranspiration [mm]` = mean(`evap`)) %>%
+    as.data.table() # convert the result to data table for future use
   
   # conduct a global linear regression analysis
   model <- lm(`Mean evapotranspiration [mm]` ~ Date, data = gleam_datatable_mean)
@@ -65,6 +65,7 @@ for(evap_data_path in evap_data_paths){
          plot = last_plot(),
          create.dir = TRUE)
   
+  
   ## Save model summary
   
   # Capture console output
@@ -74,8 +75,59 @@ for(evap_data_path in evap_data_paths){
   # Stop capturing console output
   sink()
   
-  # --- Step 7: Free the unused RAM space ---
+  # --- Step 7: Partially free the unused RAM space ---
+  # remove "model" object and "output_file_name" variable from the memory in order to free the memory and avoid any variable name conflicts in future
+  rm(model) 
+  rm(output_file_name) 
+  
+  # --- Step 8: Apply LOESS smoothing ---
+  
+  # Preparation: add a numeric date column for loess() function which works only with numeric formats
+  gleam_datatable_mean[, Date_numeric := as.numeric(Date)] 
+  
+  # Conduct a LOESS smoothing
+  model <- loess(`Mean evapotranspiration [mm]` ~ Date_numeric, 
+                 data = gleam_datatable_mean)
+  
+  
+  
+  
+  
+  # --- Step 9: Visualize ---
+  # Visualization  of a LOESS smoothing
+  
+  ggplot(gleam_datatable_mean, aes(x = Date, y = `Mean evapotranspiration [mm]`)) +
+    geom_line() +
+    geom_smooth(method = "loess", se = FALSE, color = "red") +
+    labs(x = "Date", y = "Mean evapotranspiration [mm]") +
+    theme_classic() +
+    labs(title = "LOESS smoothing of global, mean evapotranspiration trend")
+  
+  # --- Step 10: Save the results  ---
+  
+  # Get file name
+  output_file_name <- paste0("Global LOESS smoothing ", category_name)
+  # Save plot
+  ggsave(filename = paste0(output_file_name, ".png"),
+         path = PATH_RESULTS,
+         device = "png",
+         plot = last_plot(),
+         create.dir = TRUE)
+  
+  ## Save model summary
+  
+  # Capture console output
+  sink(paste0(PATH_RESULTS, output_file_name, ".txt"))
+  # print out the text results of a global linear regression analysis
+  print(summary(model))
+  # Stop capturing console output
+  sink()
+  
+  
+  
+  # --- Step 11: Free the total unused RAM space ---
   # call garbage collector to free unused RAM space
   gc()
+  
   
 }
--- R/05_sen_slope_calculation.R
+++
@@ -1,9 +0,0 @@
-##
-#Step 5: Sen's slope calculations [map]
-##
-
-source("./source/main.R")
-
-
-## TODO
-## Step 1 - get 
